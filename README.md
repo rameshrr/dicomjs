be20d32f439ed29c498c890f64fdb4cd389514d9
Ramesh R <ramesh.jetsoft@gmail.com>
2015-07-19 08:01:26 +0530

    Added some binary utils

--- README.md
+++ README.md
@@ -8,9 +8,23 @@ install via [NPM](https://www.npmjs.com/):
 # Usage
 
 ```javascript
+var dicom = require('dicomjs');
+
 fs.readFile(sample_file_path, function (err, buffer) {
     dicom.parse(buffer, function (err, dcmData) {
         /// Shows list of elements
+
+        if (!err) {
+            for (var key in dcmData.metaElements) {
+                console.log('   tag: ', key, ', value: ', dcmData.metaElements[key].value);
+            }
+
+            for (var key in dcmData.dataset) {
+                console.log('   tag: ', key, ', value: ', dcmData.dataset[key].value);
+            }
+        } else {
+            console.log(err);
+        }
     });
 });
 ```
--- test/index.js
+++ test/index.js
@@ -11,22 +11,30 @@ var dicom = require('../index'),
     assert = require('assert'),
     fs = require('fs'),
     path = require('path'),
-    sampleDcmPath = 'samples/CR-MONO1-10-chest'
+//sampleDcmPath = 'samples/CR-MONO1-10-chest'
 //sampleDcmPath = 'samples/DISCIMG/IMAGES/CRIMAGEA'
 //    sampleDcmPath = 'samples/DISCIMG/IMAGES/DXIMAGEA'A'
-//sampleDcmPath = 'samples/CT-MONO2-16-ort' /// Implicit VR
+    sampleDcmPath = 'samples/CT-MONO2-16-ort' /// Implicit VR
     ;
 
 fs.readFile(path.join(__dirname, sampleDcmPath), function (err, buffer) {
     dicom.parse(buffer, function (err, dcmData) {
         //console.log(err);
-        console.log(dcmData);
 
-        //console.log('No of elements in dataset');
         if (!err) {
-            console.log('Parsing successful..');
+            console.log('Meta Elements..');
+            for (var key in dcmData.metaElements) {
+                console.log('   tag: ', key, ', value: ', dcmData.metaElements[key].value);
+            }
+
+            console.log('Standard elements..');
+            for (var key in dcmData.dataset) {
+                console.log('   tag: ', key, ', value: ', dcmData.dataset[key].value);
+            }
+
+            console.log('Parsing complete..');
         }
 
-        assert.ok(err == null, 'Parsed successfully');
+        assert.ok(err == null, 'Parsed Results');
     });
 });
